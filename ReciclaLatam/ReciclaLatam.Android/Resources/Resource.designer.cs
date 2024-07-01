8e9aa7c13c6ae9c68aa56129f4000bc3734f3c3c
Joel Fabian Elmer Tello <joelfabianelmertello@hotmail.com>
2024-07-01 10:16:32 -0500

    UPDATE 010724

--- ReciclaLatam/ReciclaLatam.Android/Resources/Resource.designer.cs
+++ ReciclaLatam/ReciclaLatam.Android/Resources/Resource.designer.cs
@@ -20797,67 +20797,70 @@ namespace ReciclaLatam.Droid
 			public const int pinubicacion = 2131165397;
 			
 			// aapt resource value: 0x7F0700D6
-			public const int preference_list_divider_material = 2131165398;
+			public const int pinubicacionRecorrido = 2131165398;
 			
 			// aapt resource value: 0x7F0700D7
-			public const int puntos = 2131165399;
+			public const int preference_list_divider_material = 2131165399;
 			
 			// aapt resource value: 0x7F0700D8
-			public const int reci = 2131165400;
+			public const int puntos = 2131165400;
 			
 			// aapt resource value: 0x7F0700D9
-			public const int reciclajaMd = 2131165401;
+			public const int reci = 2131165401;
 			
 			// aapt resource value: 0x7F0700DA
-			public const int reciclajaMdFl = 2131165402;
+			public const int reciclajaMd = 2131165402;
 			
 			// aapt resource value: 0x7F0700DB
-			public const int recojo = 2131165403;
+			public const int reciclajaMdFl = 2131165403;
 			
 			// aapt resource value: 0x7F0700DC
-			public const int RecojoArrow = 2131165404;
+			public const int recojo = 2131165404;
 			
 			// aapt resource value: 0x7F0700DD
-			public const int RecojoCalen = 2131165405;
+			public const int RecojoArrow = 2131165405;
 			
 			// aapt resource value: 0x7F0700DE
-			public const int RecojoHora = 2131165406;
+			public const int RecojoCalen = 2131165406;
 			
 			// aapt resource value: 0x7F0700DF
-			public const int RecojoHoraact = 2131165407;
+			public const int RecojoHora = 2131165407;
 			
 			// aapt resource value: 0x7F0700E0
-			public const int RecojoIco = 2131165408;
+			public const int RecojoHoraact = 2131165408;
 			
 			// aapt resource value: 0x7F0700E1
-			public const int SesionGoogle = 2131165409;
+			public const int RecojoIco = 2131165409;
 			
 			// aapt resource value: 0x7F0700E2
-			public const int Splash = 2131165410;
+			public const int SesionGoogle = 2131165410;
 			
 			// aapt resource value: 0x7F0700E3
-			public const int test_custom_background = 2131165411;
+			public const int Splash = 2131165411;
 			
 			// aapt resource value: 0x7F0700E4
-			public const int tooltip_frame_dark = 2131165412;
+			public const int test_custom_background = 2131165412;
 			
 			// aapt resource value: 0x7F0700E5
-			public const int tooltip_frame_light = 2131165413;
+			public const int tooltip_frame_dark = 2131165413;
 			
 			// aapt resource value: 0x7F0700E6
-			public const int User = 2131165414;
+			public const int tooltip_frame_light = 2131165414;
 			
 			// aapt resource value: 0x7F0700E7
-			public const int userInfo = 2131165415;
+			public const int User = 2131165415;
 			
 			// aapt resource value: 0x7F0700E8
-			public const int VisionImg = 2131165416;
+			public const int userInfo = 2131165416;
 			
 			// aapt resource value: 0x7F0700E9
-			public const int wazelink = 2131165417;
+			public const int VisionImg = 2131165417;
 			
 			// aapt resource value: 0x7F0700EA
-			public const int youtube = 2131165418;
+			public const int wazelink = 2131165418;
+			
+			// aapt resource value: 0x7F0700EB
+			public const int youtube = 2131165419;
 			
 			static Drawable()
 			{
